2028c45e7896e1a633c738f54d18c03ade429d23
DarkClie <cervantescharls3@gmail.com>
2024-12-07 23:01:27 +0800

    Initial commit

--- includes/auth.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Start the session only if it hasn't been started already
-if (session_status() == PHP_SESSION_NONE) {
-    session_start();
-}
-
-// Include other required files, like the database connection
-require 'db.php';
-?>
